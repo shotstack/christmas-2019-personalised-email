6a5209c2c2ce84e592449bce1440e6a1ccd1de39
Jeff Shillitto <jeff@shillitto.com>
2019-12-17 22:19:08 +1100

    Post personalised template and write to file

---
+++ queue.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const request = require('request');
+const csv = require('fast-csv');
+
+const USER_LIST = './user-list.csv';
+const MAILING_LIST = './mailing-list.csv';
+const API_KEY = process.env.SHOTSTACK_KEY;
+const ENDPOINT = process.env.SHOTSTACK_ENDPOINT;
+const CUSTOMER_ID = process.env.SHOTSTACK_CUSTOMER_ID;
+const PREVIEW_URL = process.env.SHOTSTACK_PREVIEW_URL;
+const TEMPLATE = fs.readFileSync('./template.json', 'utf8');
+const fileStream = fs.createWriteStream(MAILING_LIST, { flags: 'a' });
+
+fs.createReadStream(USER_LIST)
+    .pipe(csv.parse())
+    .on('data', row => {
+        let template = JSON.parse(
+            JSON.stringify(TEMPLATE).replace('{{FIRST_NAME}}', row[1])
+        );
+
+        request({
+            headers: {
+                'x-api-key': API_KEY,
+                'Content-Type': 'application/json'
+            },
+            uri: ENDPOINT,
+            json: template,
+            method: 'POST'
+        }, function (err, res, body) {
+            if (err) {
+                throw err;
+            }
+
+            if (res.statusCode !== 201) {
+                console.log(row[0], body.response.message);
+                return;
+            }
+
+            let video = body.response.id + '.mp4';
+            fileStream.write(`${row[0]},${row[1]},${video},${PREVIEW_URL}${CUSTOMER_ID}/${video}\n`);
+        });
+    });
